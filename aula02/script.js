d911a1879ecf057da3027c9e3f63e12f413add1b
davy josias <davy.josias@gmail.com>
2025-03-04 17:26:48 -0300

    First commit

---
+++ aula02/script.js
@@ -0,0 +1,44 @@
+function adicionarTarefa() {
+
+    //Declara o valor da variável "mensagem"
+    let mensagemSucesso = "Tarefa adicionada com sucesso.";
+    let mensagemErro = "Não foi possível acidionar a tarefa!"
+
+    //Atribui à variável "inputTarefa" o elemento inputTarefa
+    const inputTarefa = document.getElementById("inputTarefa");
+    //Atribui à variavel "tarefa" o valor do elemento inputTarefa
+    let tarefa = inputTarefa.value.trim();
+    
+    const mensagem = document.getElementById("mensagem");
+
+
+    //Se houver algo no elemento inputTarefa
+    if(tarefa){
+        //console.log(tarefa);
+        mensagem.textContent = mensagemSucesso;
+        mensagem.style.color = "black";
+
+        let listaTarefas = document.getElementById("listaTarefas");
+        //console.log(listaTarefas);
+        
+        //Cria um LI e atribui á variável "novaTarefa"
+        let novaTarefa = document.createElement("li");
+
+        //Atribui ao conteúdo de "novaTarefa" o valor de "tarefa"
+        novaTarefa.textContent = tarefa;
+
+        //Insere à UL "listaTarefas" a LI "novaTarefa"
+        listaTarefas.appendChild(novaTarefa);
+
+    } else {
+        mudarPlaceholder("Não é possível adicionar uma tarefa vazia!")
+        mensagem.textContent = mensagemErro;
+        mensagem.style.color = "red";
+    }
+
+    inputTarefa.value = ""
+}
+
+function mudarPlaceholder(mensagemPlaceholder){
+    document.getElementById("inputTarefa").placeholder = mensagemPlaceholder;
+}
