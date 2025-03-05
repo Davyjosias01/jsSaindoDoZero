854e117db8b5aadda2137d47c4e7b257d17a7ac3
davy josias <davy.josias@gmail.com>
2025-03-05 15:01:51 -0300

    Commit com erro na renderização e no montante da array de tarefas

--- aula04/script.js
+++ aula04/script.js
@@ -1,66 +1,96 @@
-function adicionarTarefa() {
-    //declarando o array de tarefas com uma variável que persiste enquanto a aba estiver aberta
-    let tarefas = window.name ? JSON.parse(window.name) : [];
-    
-    
+function adicionaTarefa() {
+
     //declarando mensagens
     let mensagemSucesso = "Tarefa adicionada com sucesso.";
     let mensagemErro = "Não foi possível acidionar a tarefa!";
 
-
     //capturando os elementos necessários
     const inputTarefa = document.getElementById("inputTarefa");
-
-    const mensagem = document.getElementById("mensagem");
-    
 
     //capturando o valor no input "inputTarefa";
     let tarefa = inputTarefa.value.trim();
     let apagaTarefa = inputApagaTarefa.value.trim();
 
-
     if(tarefa){
-        mensagem.textContent = mensagemSucesso;
-        mensagem.style.color = "black";
-        tarefas.push(tarefa);
-        //alert(tarefas);
-        renderizaTarefas(tarefas);
+        //declarando o array de tarefas com uma variável que persiste enquanto a aba estiver aberta
+        let arrayTarefas = window.name ? JSON.parse(window.name) : [];
+
+        let novaTarefa = document.createElement("li");
+        novaTarefa.textContent = tarefa;
+
+        console.log("Nova tarefa = " + novaTarefa)
+
+        let botaoRemover = document.createElement("button");
+        botaoRemover.className = "remover";
+
+        console.log("Botão remover = " + botaoRemover)
+        
+        novaTarefa.appendChild(botaoRemover);
+
+        console.log("Nova tarefa = " + novaTarefa)
+
+        arrayTarefas.push(novaTarefa);
+
+
+
+        renderizaTarefas(arrayTarefas);
+
+        window.name = JSON.stringify(arrayTarefas);
+
+        alteraMensagem(mensagemSucesso, "black");
     } else {
-        document.getElementById("inputTarefa").placeholder = "Não é possível adicionar uma tarefa vazia!";
-        mensagem.textContent = mensagemErro;
-        mensagem.style.color = "red";
+        alteraMensagem(mensagemErro, "red");
     }
 
     inputTarefa.value = "";
-    window.name = JSON.stringify(tarefas);
 }
 
 function apagaTarefa(){
-    //recebe a array que persistiu na variável window.nam
-    let tarefas = window.name ? JSON.parse(window.name) : [];
+    let mensagemErro = "Insira um valor válido no campo de apagar tarefas!"
+    let mensagemSucesso = "Tarefa apagada com sucesso!"
 
     const inputApagaTarefa = document.getElementById("inputApagaTarefa");
-    const apagaTarefa = inputApagaTarefa.value;
+    const apagaTarefa = inputApagaTarefa.value.trim();
 
-    tarefas = tarefas.filter(tarefa => tarefa !== apagaTarefa);
-    //alert(tarefas)
+    if(apagaTarefa){
+        //recebe a array que persistiu na variável window.nam
+        let arrayTarefas = window.name ? JSON.parse(window.name) : [];
+        let tamanhoAntes = arrayTarefas.length;
 
-    window.name = JSON.stringify(tarefas);
+        //filtrando a array com o valor passado por parâmetro
+        arrayTarefas = arrayTarefas.filter(tarefa => tarefa !== apagaTarefa);
+        
+        if (tamanhoAntes !== arrayTarefas.length){
+            alteraMensagem(mensagemSucesso);
+            window.name = JSON.stringify(arrayTarefas);
+            renderizaTarefas(arrayTarefas);    
+        } else {
+            alteraMensagem(mensagemErro, "red");
+        }
 
-    renderizaTarefas(tarefas)
+    } else {
+        alteraMensagem(mensagemErro, "red")
+    }
+    
+    apagaTarefa = "";
 }
 
-function renderizaTarefas(tarefas){
+function alteraMensagem(mensagem){
+    const elementMensagem = document.getElementById("mensagem");
+    elementMensagem.textContent = mensagem;
+}
+
+function alteraMensagem(mensagem, cor){
+    const elementMensagem = document.getElementById("mensagem");
+    elementMensagem.textContent = mensagem;
+    elementMensagem.style.color = cor;
+}
+
+function renderizaTarefas(arrayTarefas){
     const listaTarefas = document.getElementById("listaTarefas");
     listaTarefas.innerHTML = "";
 
-    tarefas.forEach(tarefa => {
-        let novaTarefa = document.createElement("li");
-        novaTarefa.textContent = tarefa;
-        listaTarefas.appendChild(novaTarefa);
+    arrayTarefas.forEach(tarefa => {
+        listaTarefas.appendChild(tarefa.value);
     });
-}
-
-function mudarPlaceholder(mensagemPlaceholder){
-    
 }
